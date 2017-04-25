5f582687dc9f003bcf909ebd4206a54f66b0c394
Manuel Huber <nelo112@web.de>
2017-04-25 13:53:21 +0200

    Added header styling for visible/hidden state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,11 @@ import { Component, ViewEncapsulation } from '@angular/core';
  */
 @Component({
   selector: 'app',
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.Native,
   styleUrls: [
     './app.component.css'
   ],
-  template: `
-    <accordion>
-      <accordion-entry>
-        <accordion-header>heada</accordion-header>
-        <accordion-content>Eheu, regius fluctus!Impositios velum, tanquam castus decor.Ferox, audax orgias recte
-          contactus de lotus, placidus exemplar.Genetrixs persuadere in placidus virundum!Fidelis genetrixs ducunt ad
-          tolosa!Heu.Caniss sunt eposs de varius impositio.Lactea de noster lixa, amor orgia!
-        </accordion-content>
-      </accordion-entry>
-      <accordion-entry>
-        <accordion-header>heada</accordion-header>
-        <accordion-content>Eheu, regius fluctus!Impositios velum, tanquam castus decor.Ferox, audax orgias recte
-          contactus de lotus, placidus exemplar.Genetrixs persuadere in placidus virundum!Fidelis genetrixs ducunt ad
-          liberi.Hercle, bromium pius!, verpa!Abnobas accelerare!Pol.Clemens poeta semper manifestums abaculus est.Heu,
-          species!Sunt contencioes aperto germanus, fatalis lamiaes.A falsis, tabes lotus planeta.Sunt fermiumes
-          resuscitabo germanus, velox assimilatioes.Pol.Fatalis, alter demolitiones inciviliter resuscitabo de audax,
-          mirabilis epos.Detriuss manducare in clemens brema!Cur cotta credere?Galluss observare in germanus
-          tolosa!Heu.Caniss sunt eposs de varius impositio.Lactea de noster lixa, amor orgia!
-        </accordion-content>
-      </accordion-entry>
-    </accordion>
-    <div>foo</div>`
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   public visible: boolean = false;
