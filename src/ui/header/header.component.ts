281c97e4acf6915fa7583a7fc57f1dab846a15ba
Manuel Huber <nelo112@web.de>
2017-04-30 23:50:55 +0200

    Added a menu to the header

--- src/ui/header/header.component.ts
+++ src/ui/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'header',
   templateUrl: 'header.component.html',
+  encapsulation: ViewEncapsulation.Native,
   styleUrls: [ 'header.component.scss']
 })
 export class HeaderComponent {
