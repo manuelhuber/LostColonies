63a3a5a7507d7c03f8e24de14d15e2ee34fd0963
Manuel Huber <nelo112@web.de>
2017-05-05 10:53:26 +0200

    Menu closes on route change Fixed margins

--- src/ui/header/header.component.ts
+++ src/ui/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccordionEntryComponent } from '../common/accordion/entry/accordion-entry.component';
 
 @Component({
   selector: 'header',
@@ -6,6 +8,15 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Native,
   styleUrls: [ 'header.component.scss' ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
   public menuOpen : boolean;
+  @ViewChild(AccordionEntryComponent) public menu;
+
+  constructor(private route : Router) {
+  }
+
+  public ngOnInit() : void {
+    this.route.events.subscribe(() => this.menu.visible = false);
+  }
 }
