468784b27cc19c5dc6665981ad867c17209f1b3a
Manuel Huber <nelo112@web.de>
2017-05-06 16:33:19 +0200

    Added pagination

--- src/ui/common/achievements/achievements.component.ts
+++ src/ui/common/achievements/achievements.component.ts
@@ -10,29 +10,43 @@ import { ExpandableComponent } from '../expandables/expandable.component';
 export class AchievementsComponent {
   @Input() public achievements : Achievement[];
   @ViewChildren(ExpandableComponent) private expandables : QueryList<ExpandableComponent>;
-  // @Input() public maxAmount : number = 50;
-  //
-  // get achievementsToDisplay() : Achievement[][] {
-  //   return this.achievements
-  //     .reduce((rows, key, index) =>
-  //     (index % this.maxAmount === 0 ? rows.push([ key ]) : rows[ rows.length - 1 ].push(key)) && rows, []);
-  // }
-  //
-  // get showLeft() : boolean {
-  //   return true;
-  // }
-  //
-  // get showRight() : boolean {
-  //   return true;
-  // }
-  //
-  // public swipeLeft() : void {
-  // }
-  //
-  // public swipeRight() : void {
-  // }
+  @Input() public maxAmount : number = 3;
+  private translate : number = 0;
+
+  get achievementsToDisplay() : Achievement[][] {
+    return this.achievements
+      .reduce((rows, key, index) =>
+      (index % this.maxAmount === 0 ? rows.push([ key ]) : rows[ rows.length - 1 ].push(key)) && rows, []);
+  }
+
+  get translateValue() : string {
+    return 'translateX(' + this.translate + '%)';
+  }
+
+  get translateStep() : number {
+    return 100 / this.achievementsToDisplay.length;
+  }
+
+  get showLeft() : boolean {
+    return this.translate < 0;
+  }
+
+  get showRight() : boolean {
+    return this.translate > -100 + this.translateStep;
+  }
+
+  public swipeLeft() : void {
+    this.translate += this.translateStep;
+
+  }
+
+  public swipeRight() : void {
+    this.translate -= this.translateStep;
+
+  }
 
   public toggle() : void {
+    console.log(this.expandables);
     this.expandables.toArray().forEach((value) => value.visible = !value.visible);
   }
 }
