11daf766d363d9154991ef76c53d3666b74a3855
Manuel Huber <nelo112@web.de>
2017-05-06 15:12:20 +0200

    Made achievements expandable

--- src/ui/common/achievements/achievements.component.ts
+++ src/ui/common/achievements/achievements.component.ts
@@ -1,11 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, QueryList, ViewChildren } from '@angular/core';
 import { Achievement } from '../../../data/models/achievement';
+import { ExpandableComponent } from '../expandables/expandable.component';
 
 @Component({
   selector: 'achievements',
-  templateUrl: 'achievements.component.html'
+  templateUrl: 'achievements.component.html',
+  styleUrls: [ 'achievements.component.scss' ]
 })
 export class AchievementsComponent {
   @Input() public achievements : Achievement[];
-  public showMore : boolean;
+  @ViewChildren(ExpandableComponent) private expandables : QueryList<ExpandableComponent>;
+  // @Input() public maxAmount : number = 50;
+  //
+  // get achievementsToDisplay() : Achievement[][] {
+  //   return this.achievements
+  //     .reduce((rows, key, index) =>
+  //     (index % this.maxAmount === 0 ? rows.push([ key ]) : rows[ rows.length - 1 ].push(key)) && rows, []);
+  // }
+  //
+  // get showLeft() : boolean {
+  //   return true;
+  // }
+  //
+  // get showRight() : boolean {
+  //   return true;
+  // }
+  //
+  // public swipeLeft() : void {
+  // }
+  //
+  // public swipeRight() : void {
+  // }
+
+  public toggle() : void {
+    this.expandables.toArray().forEach((value) => value.visible = !value.visible);
+  }
 }
