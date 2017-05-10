e1ec82f7cf358bd6e7e8b743aee4d0b53a26b0b5
Manuel Huber <nelo112@web.de>
2017-05-10 15:37:25 +0200

    Made the map highlight more configurable

--- src/data/models/MapHighlight.ts
+++ src/data/models/MapHighlight.ts
@@ -1,7 +1,7 @@
 export class MapHighlight {
   // The coordinates in percentage distance from the top/left border where the highlight should be on the map
-  public top : number;
-  public left : number;
+  public location : { top : number, left : number };
+  public size : { x : number, y : number, rotation? : number };
   public name : string;
   public description : string;
 }
--- src/ui/common/map/map.component.ts
+++ src/ui/common/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Map } from '../../../data/models/Map';
+import { MapHighlight } from '../../../data/models/MapHighlight';
 
 @Component({
   selector: 'map',
@@ -12,5 +13,11 @@ export class MapComponent {
 
   public toggle(i : number) : void {
     this.activeDescription = this.activeDescription === i ? -1 : i;
+  }
+
+  public descriptionTopValue(highlight : MapHighlight) : string {
+    let top = highlight.location.top;
+    let heightOffset = highlight.size.y / 2;
+    return (top + (top > 50 ? -heightOffset : heightOffset)) + '%';
   }
 }
