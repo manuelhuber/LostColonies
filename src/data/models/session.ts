47ab90bfe1da562e5f8237723d4a2ca9f45ca992
Manuel Huber <nelo112@web.de>
2017-05-13 16:27:47 +0200

    Fixed data type

--- src/data/models/session.ts
+++ src/data/models/session.ts
@@ -1,6 +1,6 @@
 export class Session {
   public name : string;
-  public date : Date;
+  public date : number;
   public characters : string[];
   public description : string;
   public author : string;
