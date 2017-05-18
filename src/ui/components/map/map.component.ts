7b6a6b4e0b23ef1d7113c3b724cacb619c76dab4
Manuel Huber <nelo112@web.de>
2017-05-19 00:45:51 +0200

    Made Highlight even fancier

--- src/ui/components/map/map.component.ts
+++ src/ui/components/map/map.component.ts
@@ -6,7 +6,7 @@ import { LinkLocation } from '../../directive/insertLinks/insertLinks.directive'
 // MAGIC NUMBER WARNING: This value is dependent on the margin in the map.component.scss
 const DOUBLE_MARGIN : string = '20px';
 const BORDER_APPEAR_ANIMATION : string = 'border 500ms ease-out';
-const BORDER_DISAPPEAR_ANIMATION : string = 'border 500ms ease-in, height 0ms 500ms, width 0ms 500ms';
+const BORDER_DISAPPEAR_ANIMATION : string = 'border 500ms ease-in, height 0ms 500ms, width 0ms 500ms, transform 500ms';
 const BORDER_NO_ANIMATION : string = 'none';
 
 @Component({
@@ -19,6 +19,43 @@ export class MapComponent {
   public highlightOverlayAnimation : string;
   @Input() public map : MyMap;
   @Input() public excludeLinks : LinkLocation[];
+
+  get activeHighlight() : MapHighlight {
+    return this.map.highlights[ this.activeDescription ];
+  }
+
+  get overlayWrapperStyle() : any {
+    if (this.activeDescription !== -1) {
+      return {
+        top: this.activeHighlight.location.top + '%',
+        left: this.activeHighlight.location.left + '%',
+        width: this.activeHighlight.size.x + '%',
+        height: this.activeHighlight.size.y + '%'
+      };
+    } else {
+      return {
+        top: '50%',
+        left: '50%',
+        width: '100%',
+        height: '100%'
+      };
+    }
+  }
+
+  get overlayStyle() : any {
+    if (this.activeDescription !== -1) {
+      return {
+        transform: 'rotate(' + this.activeHighlight.size.rotation + 'deg)',
+        transition: this.highlightOverlayAnimation
+      };
+    } else {
+      return {
+        transform: 'rotate(0deg)',
+        transition: this.highlightOverlayAnimation
+      };
+    }
+
+  }
 
   public toggle(i : number) : void {
     if (this.activeDescription === i) {
@@ -46,6 +83,16 @@ export class MapComponent {
   public descriptionBottomValue(highlight : MapHighlight) : string {
     return this.isUpwards(highlight) ? 100 - this.descriptionTopNumber(highlight) + '%' : 'auto';
   }
+
+  public getLocationAndSize(highlight : MapHighlight) : any {
+    return {
+      top: highlight.location.top + '%',
+      left: highlight.location.left + '%',
+      width: highlight.size.x + '%',
+      height: highlight.size.y + '%'
+    };
+  }
+
 
   private descriptionTopNumber(highlight : MapHighlight) {
     let top = highlight.location.top;
