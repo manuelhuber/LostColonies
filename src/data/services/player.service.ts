2aaef416ea9a6afd065bb4f37d240eba65fd908d
Manuel Huber <nelo112@web.de>
2017-07-18 12:40:46 +0200

    Removed dead code
    (cherry picked from commit d2f1d9e)

--- src/data/services/player.service.ts
+++ src/data/services/player.service.ts
@@ -2,19 +2,11 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Player } from '../models/player';
 import { Gang } from '../models/gang';
 import { Session } from '../models/session';
 
-export interface GangsAndPlayers {
-  players : Player[];
-  gangs : Gang[];
-}
-
 @Injectable()
 export class PlayerService {
-
-  private data : Observable<GangsAndPlayers>;
 
   private crewOne : Observable<Gang>;
 
